25e98fe013520a008b50a78761924c5f9064961e
nemewar <frank_gb_010@hotmail.com>
2022-10-20 03:30:37 -0500

    update spinner on checkout

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -3,9 +3,12 @@ import { useContext } from "react"
 import { resolverOrden } from "../../services/firestoreOrdenes";
 import CartContext from "../context/CartContext";
 import { UserContext } from "../context/UserContext"
-import { useNavigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 
 import "./checkout.css"
+import { Spinner } from "../Spinner/Spinner";
+import { useState } from "react";
+import { useRef } from "react";
 
 export const Checkout = () => {
 
@@ -13,12 +16,16 @@ export const Checkout = () => {
     const { dataProducts, clear, precioTotal } = useContext(CartContext);
     const { user: usuario } = user;
     const navigate = useNavigate();
+    const checkoutRef = useRef();
+    const spinnerRef = useRef();
 
     const goBack = () => {
         navigate(-1)
     }
 
     const goToOrder = () => {
+        checkoutRef.current.style.display = "none"
+        spinnerRef.current.style.display = "block"
         const order = {
             date: new Date(),
             dataProducts: dataProducts,
@@ -30,64 +37,72 @@ export const Checkout = () => {
                 navigate(`/account/orders/${ordenId}`)
             })
             .catch(err => console.log(err))
+        
     }
 
     return (
         <>
             {
                 (user.logged) &&
-                <>
-                    <div className="mgc-2">
-                        <div className="contenedor-checkout">
-                            <div className="resumen">
-                                <h2>Resumen de la orden</h2>
-                                <p>PRECIO TOTAL: ${precioTotal()}</p>
-                                <p>Solicitante: {usuario.nombres + " " + usuario.apellidos}</p>
-                                <p>Correo: {usuario.correo}</p>
-                                <p>Numero: {usuario.numero}</p>
-                            </div>
-                            <h2>Productos: </h2>
-                            <table className="table-checkout" border={1}>
-                                <thead>
-                                    <tr>
-                                        <th>Producto</th>
-                                        <th>Cantidad</th>
-                                        <th>Precio</th>
-                                        <th>Total</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {
-                                        dataProducts.map(item => {
-                                            return (
-                                                <tr key={item.id}>
-                                                    <td className="producto">
-                                                        <img src={`${item.img}`}></img>
-                                                        <p>{item.nombre}</p>
-                                                    </td>
-                                                    <td>{item.cantidad}</td>
-                                                    <td>{item.precio}</td>
-                                                    <td>${item.cantidad * parseInt(item.precio.split("$")[1])}</td>
-                                                </tr>
-                                            )
-                                        })
-                                    }
-                                </tbody>
-                            </table>
-                            <div className="checkout-buttons">
-                                <button onClick={() => { goToOrder() }}>
-                                    Confirmar Compra
-                                </button>
-                                <button onClick={goBack}>
-                                    Volver
-                                </button>
-                            </div>
+                <div className="mgc-2">
+                    <div
+                        ref={checkoutRef}
+                        className="contenedor-checkout"
+                    >
+                        <div className="resumen">
+                            <h2>Resumen de la orden</h2>
+                            <p>PRECIO TOTAL: ${precioTotal()}</p>
+                            <p>Solicitante: {usuario.nombres + " " + usuario.apellidos}</p>
+                            <p>Correo: {usuario.correo}</p>
+                            <p>Numero: {usuario.numero}</p>
+                        </div>
+                        <h2>Productos: </h2>
+                        <table className="table-checkout" border={1}>
+                            <thead>
+                                <tr>
+                                    <th>Producto</th>
+                                    <th>Cantidad</th>
+                                    <th>Precio</th>
+                                    <th>Total</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    dataProducts.map(item => {
+                                        return (
+                                            <tr key={item.id}>
+                                                <td className="producto">
+                                                    <img src={`${item.img}`}></img>
+                                                    <p>{item.nombre}</p>
+                                                </td>
+                                                <td>{item.cantidad}</td>
+                                                <td>{item.precio}</td>
+                                                <td>${item.cantidad * parseInt(item.precio.split("$")[1])}</td>
+                                            </tr>
+                                        )
+                                    })
+                                }
+                            </tbody>
+                        </table>
+                        <div className="checkout-buttons">
+                            <button onClick={() => {
+                                goToOrder()
+                            }}>
+                                Confirmar Compra
+                            </button>
+                            <button onClick={goBack}>
+                                Volver
+                            </button>
                         </div>
                     </div>
-
-                </>
+                    <div
+                        style={{ display: "none" }}
+                        ref={spinnerRef}
+                    >
+                        <Spinner />
+                    </div>
+                </div>
             }
-
         </>
     )
 }
