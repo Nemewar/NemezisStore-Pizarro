89b786d5390fd096c92fce27f693acf340101509
nemewar <frank_gb_010@hotmail.com>
2022-10-28 18:59:57 -0500

    added nosotros page

--- src/components/Nosotros/Nosotros.jsx
+++ src/components/Nosotros/Nosotros.jsx
@@ -1,7 +1,22 @@
 
+import "./nosotros.css"
 
 export const Nosotros = () => {
     return (
-        <h1>NosotrosPage</h1>
+        <>
+            <div className="cont-nos">
+                <h1>Sobre nosotros</h1>
+                <div className="contenedor-nosotros">
+                    <img
+                        className="img"
+                        src="/assets/img/nosotros/consolas.jpg"
+                        alt="no encontrado"
+                    />
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At voluptate similique voluptates, itaque quasi rerum, unde doloribus quibusdam quis quisquam consequuntur, voluptatem eligendi est reiciendis! Excepturi enim debitis error atque.
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores quis minus nobis saepe molestias cumque cum architecto unde voluptate ad molestiae, explicabo est consequatur soluta, eius, atque illo neque recusandae.
+                    </p>
+                </div>
+            </div>
+        </>
     )
 }
